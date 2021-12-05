9cc26c7ca08fdf82c789f9dea40c074f3cf97994
Naveen Luhach <naveenluhach@Naveens-MacBook-Air.local>
2021-12-05 23:38:17 +0530

    2 new apis for getBalance and addBalance

--- src/main/java/com/ewallet/demo/Controller/TransactionController.java
+++ src/main/java/com/ewallet/demo/Controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.ewallet.demo.Controller;
 
+import com.ewallet.demo.Model.AddBalanceDetails;
 import com.ewallet.demo.Model.Transaction;
 import com.ewallet.demo.Model.User;
 import com.ewallet.demo.Model.Wallet;
@@ -9,12 +10,10 @@ import com.ewallet.demo.ThirdPartyServices.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import javax.ws.rs.Path;
 
 @RestController
 @RequestMapping("/transaction")
@@ -68,6 +67,28 @@ public class TransactionController {
 
     //getBalance
 
+
     //addBalance
+    //request ---- user id, amount
+    //wallet already have 100 rs, add 10
+    @PutMapping("/addBalance")
+    public Integer addBalance(@RequestBody AddBalanceDetails addBalanceDetails){
+        LOGGER.info("Got this request for adding balance : {}", addBalanceDetails.toString());
+        //find wallet by user id
+        Wallet wallet = walletRepository.findByUserId(addBalanceDetails.getUserid());
+        //set balance in the wallet
+        wallet.setBalance(addBalanceDetails.getAmount()+ wallet.getBalance());
+        walletRepository.save(wallet);
+        return wallet.getBalance();
+    }
+
+    @GetMapping("/getBalance/{userid}")
+    public Integer getBalance(@PathVariable int userid) throws Exception{
+        Wallet wallet = walletRepository.findByUserId(userid);
+        if(wallet == null){
+            throw new Exception("Wallet is not created for this user : "+userid);
+        }
+        return wallet.getBalance();
+    }
 
 }
